3111d4c75d291120adc6ff49dacbde7580812d3d
kingdle <nkings@163.com>
2018-01-31 21:36:15 +0800

    add response.php

---
+++ response.php
@@ -0,0 +1,23 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use EasyWeChat\Factory;
+
+$config = [
+	'app_id' => 'wx286efb3424d51095',
+	'secret' => '4f66978891d521a9f1772cda0e4fa670',
+
+	
+
+];
+
+$app = Factory::officialAccount($config);
+// $app = Factory::work($config);//企业号，针对企业号下的单个应用
+// $app = Factory::openPlatform($config);//开放平台
+// $app = Factory::miniProgram($config);//小程序
+// $app = Factory::payment($config);//微信支付
+
+$response = $app->user->list();
+
+var_dump($response);
