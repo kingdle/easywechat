d0db55dc8ab4881c0fe7af4347dda3b00039408b
kingdle <nkings@163.com>
2018-01-31 21:48:41 +0800

    update response.php collection

--- response.php
+++ response.php
@@ -8,16 +8,17 @@ $config = [
 	'app_id' => 'wx286efb3424d51095',
 	'secret' => '4f66978891d521a9f1772cda0e4fa670',
 
+	'log' => [
+		'file' =>__DIR__.'/easywechat.log',
+		'level' =>'debug',
+	],
+
+	'response_type' => 'collection'//array(default)/collection/object/raw/自定义类名
+
 	
 
 ];
 
 $app = Factory::officialAccount($config);
-// $app = Factory::work($config);//企业号，针对企业号下的单个应用
-// $app = Factory::openPlatform($config);//开放平台
-// $app = Factory::miniProgram($config);//小程序
-// $app = Factory::payment($config);//微信支付
 
-$response = $app->user->list();
-
-var_dump($response);
+var_dump($app->user->list());
